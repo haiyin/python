fb8f4a95a34581da4d3dde21d5e5ce31718dbd57
haiyin <qiuhaiyin@gmail.com>
2024-05-24 13:00:11 +0200

    Delete grocery directory

--- grocery/backend/server_test.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,request,jsonify
-app = Flask(__name__)
-@app.route('/hello')
-def hello():
-    return "hello kitty"
-
-if __name__ == "__main__":
-    print("gocery store")
-    app.run(port=5000)
